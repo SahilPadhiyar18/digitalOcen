52e3b71c7db682371fc52d91e1b05786bdb4e5a7
SahilPadhiyar18 <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-09-30 17:43:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Sammy!'
+    return 'Hello sahil'
+
+@app.route('/sahil', methods=['GET','POST'])
+def online():
+    try:
+        return "hello sahil"        
+    except:
+        return "data is not come"
 
 if __name__ == '__main__':
     app.run()
