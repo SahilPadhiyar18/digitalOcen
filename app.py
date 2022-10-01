7af1cf85c420240a12d0a655382cb68c8456bf27
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-10-01 14:43:38 +0530

    as

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import *
+import mysql.connector
 
+app = Flask(__name__)
+ 
 @app.route('/')
 def hello_world():
-    return 'Hello sahil'
-
-@app.route('/sahil', methods=['GET','POST'])
-def online():
-    try:
-        return "hello sahil"        
-    except:
-        return "data is not come"
-
+    mydb = mysql.connector.connect(
+    host="120.72.91.157",
+    user="admsljin_admsljinstitutes",
+    password="Adms@_#2022",
+    database="admsljin_adms_database"
+    )
+    print(mydb)
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM customers")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
+    return  render_template('home.html',name= "sahil" , data = myresult)
+ 
 if __name__ == '__main__':
+    
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run()
