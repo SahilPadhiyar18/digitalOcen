90eefc4427c2973e1306a6bcf9d2b44512f4d15b
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-10-11 10:31:11 +0530

    as

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 from flask import *
-import mysql.connector
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    mydb = mysql.connector.connect(
-    host="120.72.91.157",
-    user="admsljin_admsljinstitutes",
-    password="Adms@_#2022",
-    database="admsljin_adms_database"
-    )
-    print(mydb)
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM customers")
-    myresult = mycursor.fetchall()
-    for x in myresult:
-        print(x)
-    return  render_template('home.html',name= "sahil" , data = myresult)
+
+    client = pymongo.MongoClient("mongodb+srv://sahil:meHta9662@cluster0.48pouov.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+    db = client.test
+   
+    mydb = client["mydatabase"]
+    mycol = mydb["customers"]
+
+    x = mycol.find_one()
+
+    print(x)
+    
+
+    return  render_template('home.html',name= "sahil" , data = x)
  
 if __name__ == '__main__':
     
-#     app.jinja_env.auto_reload = True
-#     app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run()
