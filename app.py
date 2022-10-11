167a049e311090238c819207eb486359bcff856c
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-10-11 15:11:54 +0530

    as

--- app.py
+++ app.py
@@ -1,28 +1,58 @@
-from flask import *
-import pymongo
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+
+# Start with a basic flask app webpage.
+from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, url_for, copy_current_request_context
+from random import random
+from time import sleep
+from threading import Thread, Event
+
+__author__ = 'slynn'
 
 app = Flask(__name__)
- 
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+#turn the flask app into a socketio app
+socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
+
+#random number Generator Thread
+thread = Thread()
+thread_stop_event = Event()
+
+def randomNumberGenerator():
+    """
+    Generate a random number every 2 seconds and emit to a socketio instance (broadcast)
+    Ideally to be run in a separate thread?
+    """
+    #infinite loop of magical random numbers
+    print("Making random numbers")
+    while not thread_stop_event.isSet():
+        number = round(random()*10, 3)
+        print(number)
+        socketio.emit('newnumber', {'number': number}, namespace='/test')
+        socketio.sleep(2)
+
+
 @app.route('/')
-def hello_world():
+def index():
+    #only by sending this page first will the client be connected to the socketio instance
+    return render_template('index.html')
 
-    client = pymongo.MongoClient("mongodb+srv://sahil:meHta9662@cluster0.48pouov.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
-    db = client.test
-   
-    mydb = client["mydatabase"]
-    mycol = mydb["customers"]
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    # need visibility of the global thread object
+    global thread
+    print('Client connected')
 
-    x = mycol.find_one()
+    #Start the random number generator thread only if the thread has not been started before.
+    if not thread.is_alive():
+        print("Starting Thread")
+        thread = socketio.start_background_task(randomNumberGenerator)
 
-    print(x)
-    
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
 
-    return  render_template('home.html',name= "sahil" , data = x)
- 
+
 if __name__ == '__main__':
-    
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run()
+    socketio.run(app)
