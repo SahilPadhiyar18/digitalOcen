a1d48ee5e7b29f06cf4d00a8d56320887ffbfcc5
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-11-14 11:19:38 +0530

    logindata

--- app.py
+++ app.py
@@ -1,58 +1,39 @@
-
-# Start with a basic flask app webpage.
-from flask_socketio import SocketIO, emit
-from flask import Flask, render_template, url_for, copy_current_request_context
-from random import random
-from time import sleep
-from threading import Thread, Event
-
-__author__ = 'slynn'
+from re import S
+from telnetlib import STATUS
+from this import d
+from click import password_option
+from flask import redirect
+from flask import Flask, render_template, request, jsonify
+from datetime import datetime
+import json
+import os
+import psycopg2
+DATABASE_URL = "postgresql://doadmin:AVNS_Zenizxlwr33GAuLRsBv@db-postgresql-nyc1-45617-do-user-12450016-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True
-
-#turn the flask app into a socketio app
-socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
-
-#random number Generator Thread
-thread = Thread()
-thread_stop_event = Event()
-
-def randomNumberGenerator():
-    """
-    Generate a random number every 2 seconds and emit to a socketio instance (broadcast)
-    Ideally to be run in a separate thread?
-    """
-    #infinite loop of magical random numbers
-    print("Making random numbers")
-    while not thread_stop_event.isSet():
-        number = round(random()*10, 3)
-        print(number)
-        socketio.emit('newnumber', {'number': number}, namespace='/test')
-        socketio.sleep(2)
 
 
-@app.route('/')
-def index():
-    #only by sending this page first will the client be connected to the socketio instance
-    return render_template('home.html')
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    # need visibility of the global thread object
-    global thread
-    print('Client connected')
-
-    #Start the random number generator thread only if the thread has not been started before.
-    if not thread.is_alive():
-        print("Starting Thread")
-        thread = socketio.start_background_task(randomNumberGenerator)
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
-
+@app.route('/', methods=['GET', 'POST']) 
+def data_page():
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cur = conn.cursor()
+    cur.execute('SELECT name FROM logindata;')
+    data = cur.fetchall()
+    cur.execute('SELECT email FROM logindata;')
+    data1 = cur.fetchall()
+    cur.execute('SELECT password FROM logindata;')
+    data2 = cur.fetchall()
+    cur.execute('SELECT logintype FROM logindata;')
+    data3 = cur.fetchall()   
+    cur.close()
+    conn.close()
+    return render_template('logindata.html',data = data[::-1] , data1 = data1[::-1],status = data2[::-1],onby=data3[::-1])
 
 if __name__ == '__main__':
-    socketio.run(app)
+    # with app.app_context():
+        # db.create_all()
+    # db.switch.drop()
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run()
